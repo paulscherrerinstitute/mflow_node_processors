194d0899fdf8aa2890f9b185c22b0aff2f6872db
Andrej Babic <andrej.babic@psi.ch>
2017-03-09 09:31:38 +0100

    add compression option when running the writer script

--- scripts/m_write_node.py
+++ scripts/m_write_node.py
@@ -16,12 +16,20 @@ parser.add_argument("connect_address", type=str, help="Connect address for mflow
 parser.add_argument("--output_file", type=str, help="Name of output h5 file to write.")
 parser.add_argument("--rest_port", type=int, default=41001, help="Port for web interface.")
 parser.add_argument("--raw", action='store_true', help="Receive the mflow messages in raw mode.")
+parser.add_argument("--compression", default=None, choices=['lz4'], help="Incoming stream compression.")
 input_args = parser.parse_args()
 
-parameters = {"dataset_name": "entry/data/data",
-              # "compression": 32008,
-              # "compression_opts": (2048, H5_COMPRESS_LZ4)
-              }
+compression_data = {
+    "lz4": {"compression": 32008,
+            "compression_opts": (2048, H5_COMPRESS_LZ4)}
+}
+
+parameters = {"dataset_name": "data"}
+
+if input_args.compression:
+    compression_arguments = compression_data.get(input_args.compression, {})
+    # noinspection PyTypeChecker
+    parameters.update(compression_arguments)
 
 if input_args.output_file:
     parameters["output_file"] = input_args.output_file
