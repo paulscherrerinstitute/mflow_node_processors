3b41945d8e748d171a8191cbd30782911946a147
Andrej Babic <andrej.babic@psi.ch>
2017-03-10 15:00:26 +0100

    changes for mflow_nodes 0.0.4 version
    increased library version

--- tests/utils/receiver_helper.py
+++ tests/utils/receiver_helper.py
@@ -1,6 +1,7 @@
 import os
 
-from mflow_nodes.stream_node import ExternalProcessWrapper, get_zmq_listener
+from mflow_nodes.stream_node import get_processor_function, get_receiver_function
+from mflow_nodes.node_manager import ExternalProcessWrapper
 
 default_frame_shape = (4, 4)
 default_number_of_frames = 16
@@ -8,13 +9,13 @@ default_output_file = "ignore_test_output.h5"
 default_dataset_name = "entry/dataset/data"
 
 
-def setup_receiver(processor, parameters=None, connection_address="tcp://127.0.0.1:40000"):
+def setup_receiver(processor, parameters=None, connect_address="tcp://127.0.0.1:40000"):
     """
     Setup and start the receiver with default values, mainly for testing purposes.
     To be used in the setUp test method.
     :param processor: Processor instance.
     :param parameters: Processor parameters. Defaults (dataset_name, output_file) provided.
-    :param connection_address: Connection address. Default "tcp://127.0.0.1:40000".
+    :param connect_address: Connection address. Default "tcp://127.0.0.1:40000".
     :return: instance of ExternalProcessorWrapper.
     """
 
@@ -23,11 +24,13 @@ def setup_receiver(processor, parameters=None, connection_address="tcp://127.0.0
                           "output_file": default_output_file}
     process_parameters.update(parameters or {})
 
-    receiver_node = ExternalProcessWrapper(get_zmq_listener(processor=processor,
-                                                            connection_address=connection_address),
+    receiver_node = ExternalProcessWrapper(processor_function=get_processor_function(processor=processor),
+                                           receiver_function=get_receiver_function(connection_address=connect_address),
                                            initial_parameters=process_parameters,
                                            processor_instance=processor)
+
     receiver_node.start()
+
     return receiver_node
 
 
