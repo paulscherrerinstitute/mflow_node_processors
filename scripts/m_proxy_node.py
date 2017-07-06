a99fae7f861886171078b8e1b84253db5b79726c
Andrej Babic <andrej.babic@psi.ch>
2017-07-06 15:31:01 +0200

    Proxy output to logger

--- scripts/m_proxy_node.py
+++ scripts/m_proxy_node.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser
+from logging import getLogger
 
 from mflow_nodes.processors.proxy import ProxyProcessor
 from mflow_nodes.script_tools.helpers import setup_logging, add_default_arguments, start_stream_node_helper
 
+_logger = getLogger("ProxyMessage")
+
 
 def run(input_args, parameters=None):
     def print_function(message):
-        print("============= Frame %i =============" % message.get_frame_index())
-        print(message.get_header())
-        print(message.get_data())
-        print("====================================")
+        _logger.debug("============= Frame %i =============" % message.get_frame_index())
+        _logger.debug(message.get_header())
+        _logger.debug(message.get_data())
+        _logger.debug("====================================")
         return True
 
     start_stream_node_helper(ProxyProcessor(proxy_function=print_function), input_args, parameters)
