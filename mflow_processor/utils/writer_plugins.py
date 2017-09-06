0dbaa9ac07aa1e2a86d265c4b974001c54d6bec6
Andrej Babic <andrej.babic@psi.ch>
2017-09-06 10:50:31 +0200

    Plugin for writing partial image frame ids

--- mflow_processor/utils/writer_plugins.py
+++ mflow_processor/utils/writer_plugins.py
@@ -9,3 +9,17 @@ def write_frame_index_to_dataset(target_dataset):
         writer.h5_datasets.setdefault(target_dataset, []).append(message.get_frame_index())
 
     return plugin
+
+
+def write_frame_parts_index(target_dataset):
+    """
+    Create a separate dataset with frame indexes for each (upper, lower) part of the image.
+    :param target_dataset: Dataset to store the frame indexes into.
+    :return: Function to pass to the writer as a plugin.
+    """
+    def plugin(writer, message):
+        # Append the frame index to the specified dataset.
+        data = message.get_header()["frame_parts_index"]
+        writer.h5_datasets.setdefault(target_dataset, []).append(data)
+
+    return plugin
