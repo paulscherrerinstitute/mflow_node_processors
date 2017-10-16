ac6680efd6a690594f9935d7a52e80c021a68ea9
Andrej Babic <andrej.babic@psi.ch>
2017-10-16 15:26:49 +0200

    New library version
    - add px run script to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mflow_node_processors',
-    version="1.2.1",
+    version="1.2.2",
     description="mflow stream node processors",
     author='Paul Scherrer Institute',
     author_email='andrej.babic@psi.ch',
@@ -12,6 +12,7 @@ setup(
     scripts=['scripts/m_write_node.py',
              'scripts/m_write_jungfrau_node.py',
              'scripts/m_write_csax_nxsas_node.py',
+             'scripts/m_write_px_node.py',
              'scripts/m_proxy_node.py',
              "scripts/m_compression_node.py",
              "scripts/m_nxmx_node.py"],
